be483f47c60813996eea79c5f045f69dd380ad24
Saurabh Bhatnagar <saurabh.writes@gmail.com>
2020-04-06 16:26:46 -0400

    Bluetooth: Android working. Had to fix min sdk version

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>safeCOVID</Text>
+      <Text>safeCOVID bluetooth!</Text>
     </View>
   );
 }
