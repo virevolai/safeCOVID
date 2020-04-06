cfccda144dc09c621ecc6786eacd1b29d054cd42
Saurabh Bhatnagar <saurabh.writes@gmail.com>
2020-04-06 16:05:25 -0400

    Working iOS Bluetooth

---
+++ ios/react-native-ble-plx-bridge.swift
@@ -0,0 +1,12 @@
+//
+//  react-native-ble-plx-bridge.swift
+//  safeCOVID
+//
+//  Created by Saurabh Bhatnagar on 4/6/20.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+import Foundation
+
+// Created file to bridge https://github.com/Polidea/react-native-ble-plx
+
